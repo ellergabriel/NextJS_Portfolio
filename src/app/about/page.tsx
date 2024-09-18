085c5b4e49b3591d267378cb8fa2c414e784fd24
gabe <ellergabriel13@gmail.com>
2024-09-18 14:54:18 -0700

    beginning tailwind styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,6 @@ import './about.css';
 
 export default function About(){
     return(
-        <h1>you are viewing about.tsx</h1>
+        <p>Hello!</p>
     );
 }
--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100">
+    <main>
       <div className="max-w-7xl w-full">
           <p>you are viewing page.tsx</p>
       </div>
